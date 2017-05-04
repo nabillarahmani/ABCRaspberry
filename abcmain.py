5d79e0b8da97800bac4e17a3f6ebe51d07a81d0d
nabillarahmani <nabilarahmani93@gmail.com>
2017-05-04 21:02:43 +0700

    update progress until reading the field map

--- abcmain.py
+++ abcmain.py
@@ -29,31 +29,37 @@ def create_app():
 		return render_template('index.html', title='', current_page='ABC Gate Home')
 
 
-	def get_hex_string(array_of_hex):
+	def get_hex_string(array_of_hex, start, end):
 		"""
+		input	:
+		return	: string representation of the given input
 		"""
 		hex_field_map_string = ""
 		for x in array_of_hex:
-			hex_field_map_string += (str)hex(x)
-		return hex_field_map_string
+			x = str(x)
+			hex_field_map_string += x[2:]
+		return hex_field_map_string[start:end]
 
 
-	def parse_field_map(field_map):
+	def get_array_hex(array_of_dec):
 		"""
-		@input : array of data 
+		input	: receives input array of decimal from responds
+		return	: return array of hex from the respond
+		"""
+		arr_hex = []
+		for data in array_of_dec:
+			arr_hex.append(hex(data))
+		return arr_hex
+
+
+	def parse_field_map(hex_string):
+		"""
+		@input : get 
 		@return: string of binary
 		"""
-		hex_field_map_string = get_hex_string(field_map)
-
-		h_size = len(hex_field_map_string) * 4
-		string_binary_field = (bin(int(hex_field_map_string, 16))[2:] ).zfill(h_size)
+		h_size = len(hex_string) * 4
+		string_binary_field = (bin(int(hex_string, 16))[2:] ).zfill(h_size)
 		return string_binary_field
-
-
-	def substring_hex(string, start, length):
-		"""
-		"""
-		return string[start*2:length*2]
 	
 
 	def verification_document(extract_fingerprint, extract_picture):
@@ -130,21 +136,17 @@ def create_app():
 			data, sw1, sw2 = connection.transmit(APDU_CARD)			
 
 			# GET CARD TYPE (XIRCA / DAM)
-			card_id = "0x" + get_hex_string(data)
-			card_id_int = int(card_id, 0)
+			card_id = data[1]
+
 
 			# SELECT EF (FIELD MAP + LENGTH MAP)
-			APDU_FIELD = [0x00, 0xA4, 0x00, 0x00, 0x02, 0x01, 0x00]
-			data, sw1, sw2 = connection.transmit(APDU_FIELD)
+			APDU_FIELD 		= [0x00, 0xA4, 0x00, 0x00, 0x02, 0x01, 0x00]
+			data, sw1, sw2 	= connection.transmit(APDU_FIELD)
 
 			#Get field map + length map
-			APDU_GET_FIELD = [0x00, 0xB0, 0x00, 0x00, 0x33]
-			data, sw1, sw2 = connection.transmit(APDU_GET_FIELD)
-
-			# Parse Field Map to check whether it is empty or not
-			# Field map = 10101010100011 (in binary)
-			field_map = parse_field_map(data)
-			flag_empty = False
+			APDU_GET_FIELD 	= [0x00, 0xB0, 0x00, 0x00, 0x33]
+			data, sw1, sw2 	= connection.transmit(APDU_GET_FIELD)
+			flag_empty 		= False	
 
 			# Check for each element whether it is filled yet or not
 			for element in data:
@@ -153,21 +155,35 @@ def create_app():
 				if converted_dec != '0xFF':
 					flag_empty = True
 
-
 			if flag_empty == True:
 				#sdcard disconnect
 				return render_template('failed_readcard.html')
+			# get the hex representation of respond
+			# parse the first 3 bits from the respond to check the length
+			# Field map = 10101010100011 (in binary) contains 3 bytes of binary representation
+			respond_field_map 	= get_array_hex(data)
+			field_map 			= parse_field_map(get_hex_string(respond_field_map, 0, 6))
 
 			# Get the total length of map
-			total_length_map = 0
-			for x in data:
-				total_length_map += x
+			total_length_map= 0
+			length_map 		= data[6:]
+			
+			for length in length_map:
+				total_length_map += length
 
-			# SELECT EF FOR FIRST DATA
+			# SELECT EF FOR FIRST DATA 
 			APDU_SELECT_DATA_1 = [0x00, 0xA4, 0x00, 0x00, 0x02, 0x01, 0x01]
 			data, sw1, sw2 = connection.transmit(APDU_SELECT_DATA_1)			
 
-			
+			# handle if content inside card is null
+			if sw1 == None and sw2 == None:
+				#sdcard disconnect
+				return render_template('failed_readcard.html')
+
+			iteration = total_length_map / 51
+
+
+
 
 		except:
 			return render_template('failed_readcard_content.html')
