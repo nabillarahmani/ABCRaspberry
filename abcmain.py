b39baeb84c537fcf301e8add379f44a33be277e2
Citra Glory <citraglory@gmail.com>
2017-06-02 14:28:26 +0700

    add latest implementation

--- abcmain.py
+++ abcmain.py
@@ -507,11 +507,15 @@ def verification_process():
 
 def take_image():
 	import os
+	import time
+	import datetime 
 	"""
 		This method will take the picture of the traveller
 	"""
+	ts = time.time()
+	ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H:%M:%S')
 	app.logger.debug('Taking picture now! Say cheese!!')
- 	os.system("sudo fswebcam --fps 15 -S 20 -s brightness=80% -r 512x384 -q data/traveller.jpg")
+ 	os.system("sudo fswebcam --fps 15 -S 20 -s brightness=80% -r 100x100 --no-banner -q ./data_logging/"+ts+".jpeg")
  	return 
 
 
@@ -525,31 +529,24 @@ def logging():
 		This method will do the logging
 	"""
 	app.logger.debug('Accessing logging route')
-	data_logging = {'identification_number': '', 'full_name': '', 'photo_taken' : '', 'status_cekal' : False}
+	data_logging = {'identification_number': '', 'full_name': '', 'photo_taken' : '', 'status_verification_cekal' : False, 'status_verification_fingerprint': False}
 	
 	if 'identification_number' in session:
 		data_logging['identification_number'] = session['identification_number']
 	if 'fullname' in session:
 		data_logging['fullname'] = session['fullname']
-	# Get the data for status cekal
 	if 'status_cekal' in session:
-		data_logging['status_cekal'] = session['status_cekal']
-	# Get the data for photo traveller
-	photo_taken = ''
-	if os.path.isfile(url + 'traveller.jpg'):
-		photo_taken = get_data_from_file(url + 'traveller.jpg')
-	else:
-		# show error
-		pass 
-	data_logging['photo_taken'] = photo_taken
-	print(len(photo_taken))
+		data_logging['status_verification_cekal'] = session['status_cekal']
+	if 'verifikasi_fingerprint' in session:
+		data_logging['status_verification_fingerprint'] = session['verifikasi_fingerprint']
 	# Get current timestamp
 	ts = time.time()
 	ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 	data_logging['timestamp_traveller'] = ts
 	app.logger.debug('ready to send request this : {}'.format(data_logging))
 	try:
-		result = requests.post("http://webservice-abcsystem.herokuapp.com/logging_data", data=data_logging)
+		result = requests.post("http://webservice-abcsystem.herokuapp.com/logging_data", params=data_logging)
+		app.logger.debug(result.url)
 		if result.status_code == 200:
 			app.logger.debug('SUCCEED SEND DATA LOGGING')
 		else:
@@ -599,10 +596,9 @@ def get_camera_data():
 	take_image()
 	# If verification succeed
 	if session['verifikasi_fingerprint'] and not session['status_cekal']:
-		return redirect(url_for('logging'))
+		return redirect(url_for('open_gate'))		
 	else:
-		return redirect(url_for('open_gate'))
-
+		return redirect(url_for('logging'))
 
 
 @app.route("/open_gate")
--- haha.py
+++ haha.py
@@ -1,50 +1,6 @@
-# import os
-
-# url = 'data/'
-# data = ''
-# if os.path.isfile(url +'information_taken.txt'):
-# 	f = open(url +'information_taken.txt')
-# 	data = f.read()
-# 	f.close
-
-# print(data)
-# print(data.split("\n"))
-# smartcard_extract = {}
-# for data in data.split("\n"):
-# 	smartcard_extract[data.split(': ')[0]] = data.split(': ')[1]
-# print(smartcard_extract)
-
-# import os, os.path
-# import requests
-import datetime
 import time
-# """
-# 	This method will do the logging
-# """
-# # TODO IMPLEMENT LOGGING INTO THE SERVER LOGGING
-# data_logging = {'identification_number': '', 'full_name': '', 'photo_taken' : '', 'status_cekal' : False}
-
-# if 'identification_number' in session:
-# 	data_logging['identification_number'] = session['identification_number']
-# if 'fullname' in session:
-# 	data_logging['fullname'] = session['fullname']
-# # Get the data for status cekal
-# if 'status_cekal' in session:
-# 	data_logging['status_cekal'] = session['status_cekal']
-# # Get the data for photo traveller
-# photo_taken = ''
-# if os.path.isfile(url + 'traveller.jpg'):
-# 	photo_taken = get_data_from_file(url + 'traveller.jpg')
-# else:
-# 	# show error
-# 	pass 
-# data_logging['photo_taken'] = photo_taken
-
-# Get current timestamp
+import datetime
 ts = time.time()
-ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-print(datetime.datetime.utcnow)
-# data_logging['timestamp_traveller'] = ts
+ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H:%M:%S')
 
 print(ts)
